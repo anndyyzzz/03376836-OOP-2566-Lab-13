6dc79a3c81fd7df02f000ac19ff8ab0c29dccac7
anndyyz <anda2546r@gmail.com>
2024-03-30 22:51:50 +0700

    3

--- 65030276_Lab13/obj/Debug/net8.0/65030276_Lab13.AssemblyInfo.cs
+++ 65030276_Lab13/obj/Debug/net8.0/65030276_Lab13.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("65030276_Lab13")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9556620de35ca88f6b0cf41029218ebe21e278e5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cd61c849234e60199aa8645f13d8269a1091dcc1")]
 [assembly: System.Reflection.AssemblyProductAttribute("65030276_Lab13")]
 [assembly: System.Reflection.AssemblyTitleAttribute("65030276_Lab13")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
