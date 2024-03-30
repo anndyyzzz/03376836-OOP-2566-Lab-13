03d927eeb0a8dcfe50a9b90c583d05918809486c
anndyyz <anda2546r@gmail.com>
2024-03-30 23:00:34 +0700

    06

---
+++ 65030276_Lab13/Lab13_Ex06/Program.cs
@@ -0,0 +1,51 @@
+﻿Television myTV = new SonyTV();
+myTV.Wattage = 100;
+myTV.TurnOn();
+myTV.ChannelUp();
+myTV.ChannelDown();
+myTV.TurnOff();
+
+Lamp myLamp = new DesktopLamp();
+myLamp.Wattage = 50;
+myLamp.TurnOn();
+myLamp.TurnOff();
+
+interface IRemoteControl
+{
+    void TurnOn();
+    void TurnOff();
+    void ChannelUp();
+    void ChannelDown();
+}
+
+abstract class PowerAppliance
+{
+    public bool PowerStatus;
+    public int Wattage;
+}
+
+abstract class Television : PowerAppliance, IRemoteControl
+{
+    public int Channel { get; set; }
+
+    public void TurnOn() { Console.WriteLine("TV Turn on"); PowerStatus = true; }
+    public void TurnOff() { Console.WriteLine("TV Turn off"); PowerStatus = false; }
+    public void ChannelUp() { Console.WriteLine("TV Channel up"); }
+    public void ChannelDown() { Console.WriteLine("TV Channel down"); }
+}
+
+abstract class Lamp : PowerAppliance, IRemoteControl
+{
+    public void TurnOn() { Console.WriteLine("Lamp Turn on"); PowerStatus = true; }
+    public void TurnOff() { Console.WriteLine("Lamp Turn off"); PowerStatus = false; }
+    public void ChannelUp() { Console.WriteLine("Lamp cannot change channel"); }
+    public void ChannelDown() { Console.WriteLine("Lamp cannot change channel"); }
+}
+
+class SonyTV : Television
+{
+}
+
+class DesktopLamp : Lamp
+{
+}
